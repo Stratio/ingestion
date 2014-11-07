aaf401f3bc6a717f4091083db4d8cb83cf51d61d
gasparms <gmunoz@stratio.com>
2014-11-07 14:06:30 +0100

    Fix issue #25

--- stratio-sources/stratio-rest-source/src/test/java/com/stratio/ingestion/source/rest/RestSourceTest.java
+++ stratio-sources/stratio-rest-source/src/test/java/com/stratio/ingestion/source/rest/RestSourceTest.java
@@ -15,15 +15,113 @@
  */
 package com.stratio.ingestion.source.rest;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyObject;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import org.apache.flume.Event;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerContext;
+import org.quartz.SchedulerException;
 
 @RunWith(JUnit4.class)
 public class RestSourceTest {
 
-    @Test
-    public void test() {
+    Client client = mock(Client.class);
+    WebTarget wt = mock(WebTarget.class);
+    Builder builder = mock(Builder.class);
+    Response response = mock(Response.class);
+    JobExecutionContext context = mock(JobExecutionContext.class);
+    Scheduler scheduler = mock(Scheduler.class);
+    SchedulerContext schedulerContext = mock(SchedulerContext.class);
+    LinkedBlockingQueue<Event> queue ;
 
+
+    @Before
+    public void setUp() throws SchedulerException {
+        when(builder.header(anyString(), anyObject())).thenReturn(builder);
+        when(wt.request(any(MediaType.class))).thenReturn(builder);
+        when(builder.get()).thenReturn(response);
+        when(builder.header(anyString(), anyObject())).thenReturn(builder);
+        when(wt.request(any(MediaType.class))).thenReturn(builder);
+        when(builder.get()).thenReturn(response);
+        when(context.getScheduler()).thenReturn(scheduler);
+        when(scheduler.getContext()).thenReturn(schedulerContext);
+        queue = new LinkedBlockingQueue<Event>(10);
+        when(schedulerContext.get("queue")).thenReturn(queue);
+        when(schedulerContext.get("client")).thenReturn(client);
     }
+
+   @Test
+    public void testSimpleGet() throws JobExecutionException {
+        String goodUrl = "GOOD URL";
+        when(client.target(goodUrl)).thenReturn(wt);
+
+        //Create Properties
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(RestSource.CONF_URL, goodUrl);
+        properties.put(RestSource.CONF_HEADERS, "{}");
+
+        when(response.readEntity(String.class)).thenReturn("XXXXX");
+
+        //Return headers
+        MultivaluedMap<String, String> responseHeaders = new MultivaluedHashMap<String, String>();
+        when(response.getStringHeaders()).thenReturn(responseHeaders);
+        when(schedulerContext.get("properties")).thenReturn(properties);
+
+        RequestJob job = new RequestJob();
+        job.execute(context);
+
+        assertEquals(queue.size(), 1);
+        assert(new String(queue.poll().getBody()).equals("XXXXX"));
+    }
+
+
+    @Test
+    public void testMultivaluedHeader() throws JobExecutionException {
+        String goodUrl = "GOOD URL";
+        when(client.target(goodUrl)).thenReturn(wt);
+
+        //Create Properties
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(RestSource.CONF_URL, goodUrl);
+        properties.put(RestSource.CONF_HEADERS, "{}");
+
+        when(response.readEntity(String.class)).thenReturn("XXXXX");
+
+        //Return headers
+        MultivaluedMap<String, String> responseHeaders = new MultivaluedHashMap<String, String>();
+        responseHeaders.put("GOODHEADER", Arrays.asList("aa", "bb"));
+        when(response.getStringHeaders()).thenReturn(responseHeaders);
+        when(schedulerContext.get("properties")).thenReturn(properties);
+
+        RequestJob job = new RequestJob();
+        job.execute(context);
+
+        assertEquals(queue.size(), 1);
+        assertEquals(queue.poll().getHeaders().get("GOODHEADER"), "aa, bb");
+    }
+
 }
