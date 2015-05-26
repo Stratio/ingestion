161193cf9812ce4ad02d03dc987c63b582de902e
dcmorell <dcastro@stratio.com>
2015-05-26 09:42:31 +0200

    pruebas local streaming

---
+++ detector/bin/run_flume-local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+INGESTION_HOME="${INGESTION_HOME:-/opt/sds/ingestion}"
+export FLUME_JAVA_OPTS="-Xmx1024m"
+cd "$(dirname $0)/../"
+exec "${INGESTION_HOME}/bin/flume-ng" agent --conf ./conf --conf-file ./conf/flume-conf-local.properties --name a -Dflume.monitoring.type=http -Dflume.monitoring.port=34545
