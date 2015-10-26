de1861bc3a3841749148d46880822c416c9ccb66
witokondoria <witokondoria@gmail.com>
2015-10-26 12:58:13 +0100

    added acceptance tests skeleton

---
+++ testAT/src/test/java/com/stratio/ingestion/testsAT/specs/Common.java
@@ -0,0 +1,7 @@
+package com.stratio.ingestion.testsAT.specs;
+
+import com.stratio.specs.CommonG;
+
+public class Common extends CommonG {
+
+}
---
+++ testAT/src/test/java/com/stratio/ingestion/testsAT/specs/WhenSpec.java
@@ -0,0 +1,9 @@
+package com.stratio.ingestion.testsAT.specs;
+
+public class WhenSpec extends BaseSpec {
+
+    public WhenSpec(Common spec) {
+        this.commonspec = spec;
+    }
+
+}
