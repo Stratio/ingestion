6e777411603faedda221403daeb13ed7f7e3d3e7
Aitor <aaitor@gmail.com>
2016-03-29 12:58:40 +0200

    fixed init scripts, INGESTION-276

--- stratio-ingestion-dist/src/main/resources/bin/ingestion.sh
+++ stratio-ingestion-dist/src/main/resources/bin/ingestion.sh
@@ -2,7 +2,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Get options
-while getopts "c:f:n:p:l:j:" option; do
+while getopts "c:f:n:p:l:j:D:" option; do
    case $option in
       c)      CONF=$OPTARG ;;
       f)      CONF_FILE=$OPTARG ;;
@@ -10,8 +10,8 @@ while getopts "c:f:n:p:l:j:" option; do
       p)      PIDFILE=$OPTARG ;;
       l)      LOG_LEVEL=$OPTARG ;;
       j)      LOG4J_FILE=$OPTARG ;;
-      *)      echo "Unknown option" ; exit 1 ;;
+      D)      INGESTION_OPTS="$INGESTION_OPTS -D${OPTARG}" ;;
    esac
 done
 
-$DIR/flume-ng agent --conf $CONF --conf-file $CONF_FILE --name $FLUME_NAME -Dlog4j.configuration=file://$LOG4J_FILE -Dflume.root.logger=$LOG_LEVEL >/dev/null 2>&1 & echo $! >$PIDFILE
+$DIR/flume-ng agent --conf $CONF --conf-file $CONF_FILE --name $FLUME_NAME -Dlog4j.configuration=file://$LOG4J_FILE -Dflume.root.logger=$LOG_LEVEL $INGESTION_OPTS >/dev/null 2>&1 & echo $! >$PIDFILE
